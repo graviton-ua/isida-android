5f73c2270a014dbe39a985d1a0fc40c5da8b41ef
Andrew Khloponin <hospes.a@gmail.com>
2022-08-05 11:24:46 +0300

    Bump Jetpack Compose version to stable 1.2.0 version and related dependencies

--- app/src/main/java/ua/graviton/isida/ui/scan/ScanDevicesActivity.kt
+++ app/src/main/java/ua/graviton/isida/ui/scan/ScanDevicesActivity.kt
@@ -83,6 +83,7 @@ class ScanDevicesActivity : AppCompatActivity() {
         }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
