04c97e16e5569e83fcb07bf9360c784c571fd362
Evgeniy-Khl <graviton.ua@gmail.com>
2026-01-28 15:49:49 +0200

    Add debugging logs all over PacketParser

--- data/repos/src/commonMain/kotlin/ua/graviton/isida/data/parsers/PacketParser.kt
+++ data/repos/src/commonMain/kotlin/ua/graviton/isida/data/parsers/PacketParser.kt
@@ -1,9 +1,12 @@
 package ua.graviton.isida.data.parsers
 
+import co.touchlab.kermit.Logger
 import com.whoppah.util.CRC16
 import com.whoppah.util.readU16LE
 import com.whoppah.util.readU8
 import ua.graviton.isida.data.protocol.packets.IsidaPacket
+
+private val logger by lazy { Logger.withTag("PacketParser") }
 
 interface PacketParser<T : IsidaPacket> {
     val version: Int
@@ -17,25 +20,38 @@ interface PacketParser<T : IsidaPacket> {
         val endSize = 2
         val expectedSize = dataOffset + length + crcSize + endSize
 
-        if (data.size < expectedSize) return false
+        if (data.size < expectedSize) {
+            logger.d { "Wrong size | expected: $expectedSize | actual size: ${data.size}" }
+            return false
+        }
 
         // Check length of data (2 bytes)
         val packetLength = data.readU16LE(2)
-        if (packetLength != length) return false
+        if (packetLength != length) {
+            logger.d { "Packet length | expected: $length | received: ${packetLength}" }
+            return false
+        }
 
         // Check command ID (1 byte)
         val packetCmdId = data.readU8(4)
-        if (packetCmdId != commandId) return false
+        if (packetCmdId != commandId) {
+            logger.d { "Command ID | expected: $commandId | received: ${packetCmdId}" }
+            return false
+        }
 
         // Check version of the packet (1 byte)
-        if (data.readU8(5) != version) return false
+        if (data.readU8(5) != version) {
+            logger.d { "Version | expected: $version | received: ${data.readU8(5)}" }
+            return false
+        }
 
         // 6. Check CRC
         // CRC covers [Start, Version, Length, CommandId, Data...]
         // From index 0 to (dataOffset + length) exclusive of CRC
-        val dataForCrc = data.copyOfRange(0, dataOffset + length)
+        val dataForCrc = data.copyOfRange(6, dataOffset + length)
         val calculatedCrc = CRC16.crcSimple(dataForCrc)
         val packetCrc = data.readU16LE(dataOffset + length)
+        logger.d { "CRC | calculated: $calculatedCrc | received: ${packetCrc}" }
 
         // crcSimple returns an Int which might use more than 16 bits.
         // SendPackageDto uses .toShort() which truncates to lower 16 bits.
