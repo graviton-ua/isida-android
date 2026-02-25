8c82f64e39667bbcfd44beeb9b1489f77ac92ffe
Evgeniy-Khl <graviton.ua@gmail.com>
2026-02-25 17:11:27 +0200

    Revert "fix: add missing 'dp' import in HomeScreen.kt"
    This reverts commit 51001d480c634f30bb313f947f34b2586fb85082.

--- ui/home/base/src/commonMain/kotlin/ua/isida/ui/home/HomeScreen.kt
+++ ui/home/base/src/commonMain/kotlin/ua/isida/ui/home/HomeScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation3.runtime.NavKey
